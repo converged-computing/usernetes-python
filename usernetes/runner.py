7aa60cabf5904e0cc0f89a5e19255494ecc332a4
vsoch <vsoch@users.noreply.github.com>
2024-12-10 23:20:22 -0700

    build environment
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- usernetes/runner.py
+++ usernetes/runner.py
@@ -226,7 +226,7 @@ class UsernetesRunner:
         """
         with utils.workdir(self.workdir):
             logger.debug(" ".join(command))
-            result = utils.run_command(command, stream=True, environ=self.environ)
+            result = utils.run_command(command, stream=True, environ=self.envars)
 
         # Assume we don't need to return the return code
         # can change if needed
